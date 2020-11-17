b07e674d36ae2a387c005e1d8f0b1a9019b01d0f
Ricardo Ruiz <gaanzz11@gmail.com>
2020-11-17 02:37:26 -0600

    fix: Agregar alumno

--- taekwondo/src/app/components/alumno/alumno.component.ts
+++ taekwondo/src/app/components/alumno/alumno.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, ɵbypassSanitizationTrustResourceUrl } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Alumno } from 'src/app/_models/alumno';
 import { AlumnoService } from 'src/app/_services/alumno.service';
 
 import Swal from 'sweetalert2';
-
-declare var $: any;
 
 @Component({
   selector: 'app-alumno',
@@ -20,7 +18,6 @@ export class AlumnoComponent implements OnInit {
   alumnoForm: FormGroup;
   imagen: File;
   submitted = false;
-  modalTitle: String;
 
   constructor(private alumnoService: AlumnoService, private formBuilder: FormBuilder) { }
 
@@ -31,7 +28,7 @@ export class AlumnoComponent implements OnInit {
       apellidos: ['', Validators.required],
       fecha_nacimiento: ['', Validators.required],
       fotografia: [''],
-      actividad: ['', Validators.required],
+      actividad_marcial: ['', Validators.required],
       grado: ['', Validators.required],
       seguro_medico: [''],
       certificado_medico: [''],
@@ -45,8 +42,7 @@ export class AlumnoComponent implements OnInit {
 
   // Consultar lista de alumnos
   getAlumnos(){
-    this.alumnos = [new Alumno(1, "Kevin", "Villegas", "11-10-1997", "Foto", "Tae Kwon Do", "Cinta Negra", "Seguro M", "Certificado M", "Carta R", "Pass", "Mail"),
-                    new Alumno(2, "Ricardo", "Salvador", "10-11-1997", "Fotografia", "Kick Boxing", "Cinta Morada", "Seguro", "Certificado", "Carta", "Password", "Email")];
+    this.alumnos = [];
     this.alumnoService.getAlumnos().subscribe(
       res => {
         this.alumnos = res;
@@ -69,100 +65,55 @@ export class AlumnoComponent implements OnInit {
 
   // Eliminar un alumno
   deleteAlumno(id){
-    Swal.fire({
-      title: 'Eliminar Alumno',
-      text: '¿Estás seguro que deseas eliminar al alumno?',
-      showDenyButton: false,
-      showCancelButton: true,
-      confirmButtonText: 'Eliminar',
-      denyButtonText: 'No eliminar',
-    }).then((result) => {
-      if(result.isConfirmed){
-        this.alumnoService.deleteAlumno(id).subscribe(
-          res => {
-            Swal.fire(
-              'Eliminado!',
-              'El alumno ha sido eliminado',
-              'success'
-            )
-            $("#alumnoModal").modal("hide");
-            this.getAlumnos();
-          },
-          err => console.error(err)
-        )
-      }
-    });
+    this.alumnoService.deleteAlumno(id).subscribe(
+      res => {
+        this.getAlumnos();
+      },
+      err => console.error(err)
+    )
   }
 
   // Crear un alumno
   onSubmit(){
     this.submitted = true;
-
+     console.log(this.alumnoForm)
     if(this.alumnoForm.invalid){
       console.log('Formulario inválido');
       return;
     }
 
-    if(this.modalTitle == "Registrar"){
-      this.alumnoService.createAlumno(this.alumnoForm.value).subscribe(
-        res => {
-          Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'El alumno ha sido registrado',
-            showConfirmButton: false,
-            timer: 1500
-          })
-          $("#alumnoModal").modal("hide");
-          this.getAlumnos();
-          this.submitted = false;
-        },
-        err => console.error(err)
-      )
-    }else{
-      console.log(this.alumnoForm.value);
-      this.alumnoService.updateAlumno(this.alumnoForm.value).subscribe(
-        res => {
-          Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'El alumno ha sido actualizado',
-            showConfirmButton: false,
-            timer: 1500
-          })
-          $("#alumnoModal").modal("hide");
-          this.getAlumnos();
-          this.submitted = false;
-        },
-        err => {
-          console.error(err);
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Error al conectar con el servidor'
-          })
-        }
-      )
-    }
+    this.alumnoService.createAlumno(this.alumnoForm.value).subscribe(
+      res => {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'El alumno ha sido creado',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.getAlumnos();
+        this.submitted = false;
+      },
+      err => console.error(err)
+    )
   }
 
   // Actualizar un alumno
   updateAlumno(alumno: Alumno){
     this.submitted = true;
 
+    this.alumnoForm.controls['id_alumno'].setValue(alumno.id_alumno);
     this.alumnoForm.controls['nombre'].setValue(alumno.nombre);
     this.alumnoForm.controls['apellidos'].setValue(alumno.apellidos);
     this.alumnoForm.controls['fecha_nacimiento'].setValue(alumno.fecha_nacimiento);
     this.alumnoForm.controls['fotografia'].setValue(alumno.fotografia);
-    this.alumnoForm.controls['actividad'].setValue(alumno.actividad);
+    this.alumnoForm.controls['actividad_marcial'].setValue(alumno.actividad_marcial);
     this.alumnoForm.controls['grado'].setValue(alumno.grado);
     this.alumnoForm.controls['seguro_medico'].setValue(alumno.seguro_medico);
     this.alumnoForm.controls['certificado_medico'].setValue(alumno.certificado_medico);
     this.alumnoForm.controls['carta_responsiva'].setValue(alumno.carta_responsiva);
+    this.alumnoForm.controls['password'].setValue(alumno.password);
     this.alumnoForm.controls['email'].setValue(alumno.email);
-
-    this.modalTitle = alumno.nombre + " " + alumno.apellidos + " (" + alumno.id_alumno + ")";
-    $("#alumnoModal").modal("show");
   }
 
   imagenSelected(event){
@@ -182,10 +133,4 @@ export class AlumnoComponent implements OnInit {
   }
 
   get f() { return this.alumnoForm.controls; }
-
-  openModalAlumno(){
-    this.alumnoForm.reset();
-    this.modalTitle = "Registrar";
-    $("#alumnoModal").modal("show");
-  }
 }
