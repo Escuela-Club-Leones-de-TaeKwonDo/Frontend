f0879e39af4881d822c89e47a6e3ae10f1c0beb0
kevRicardo <kevin.ricardo@ciencias.unam.mx>
2020-11-29 23:55:01 -0600

    cambios

--- taekwondo/src/app/_models/evento.ts
+++ taekwondo/src/app/_models/evento.ts
@@ -7,4 +7,15 @@ export class Evento {
     fecha_fin: Date;
     costo: number;
     enlace_facebook: String;
+
+    constructor(id_evento, nombre, id_tipo_evento, descripcion, fecha_inicio, fecha_fin, costo, enlace_facebook){
+        this.id_evento = id_evento;
+        this.nombre = nombre;
+        this.id_tipo_evento = id_tipo_evento;
+        this.descripcion = descripcion;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.costo = costo;
+        this.enlace_facebook = enlace_facebook;
+    }
 }
